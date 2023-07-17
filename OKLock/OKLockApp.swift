248c2358f1d222fd139bb620a48d4e59afb7c9a0
Yuga Samuel <yugasamuel2002@gmail.com>
2023-07-17 20:53:13 +0700

    First commit

---
+++ OKLock/OKLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKLockApp.swift
+//  OKLock
+//
+//  Created by Yuga Samuel on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct OKLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
