74777af7a15b3eb010b9da748b664a770fe1f9c3
Yuga Samuel <yugasamuel2002@gmail.com>
2023-07-18 12:02:49 +0700

    Re Add ContentVIew

--- OKLock/Views/Pages/ContentView.swift
+++ OKLock/Views/Pages/ContentView.swift
@@ -41,16 +41,14 @@ struct ContentView: View {
                 }
                 
             }
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environmentObject(RentViewModel())
     }
 }
+
+
